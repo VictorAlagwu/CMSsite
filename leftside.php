1f3400cefe156a977f47d72fa9298200aecb9e2d
Victor Alagwu <victoralagwu@gmail.com>
2016-09-22 11:47:26 +0100

    Add files via upload

---
+++ leftside.php
@@ -0,0 +1,8 @@
+<div class="list-group">
+					<a href="index.php" class="list-group-item active">Dashboard</a>
+					<a href="#" class="list-group-item">
+					<span class="badge">14</span><i class="fa fa-file-text"></i>All Posts</a>
+					<a href="#" class="list-group-item"><span class="badge">9</span><i class="fa fa-comment"></i>Comments</a>
+					<a href="categories.php" class="list-group-item"><span class="badge">4</span><i class="fa fa-folder-open"></i>Categories</a>
+					<a href="users.php" class="list-group-item"><span class="badge">10</span><i class="fa fa-users"></i>Users</a>
+				</div>
