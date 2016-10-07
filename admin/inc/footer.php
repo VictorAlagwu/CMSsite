623ff506c2f24a66ce5e1dde687f16ff05af12b0
Vstuffs <victoralagwu@gmail.com>
2016-10-07 11:04:11 +0100

    update

---
+++ admin/inc/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+	<div class="container">
+		Copyright &copy; by <a href="https://facebook.com/victor.alagwu">Victor Alagwu.</a> All Rights Reserved 2016.
+	</div>
+</footer>
